e6edb18d9ae899289c01aa74ea6c3cf1f049a5f2
Strick4888 <benjaminstrick2016@u.northwestern.edu>
2015-05-24 22:00:00 -0500

    made it so that you can't submit the category 1 and 2 questions if you havn't answered all of them

--- app/example/scripts/ReportController copy.js
+++ app/example/scripts/ReportController copy.js
@@ -9,6 +9,13 @@ angular
     $scope.$apply();
 
     $scope.reportUTI=function(){
+
+        if ($scope.Category.pregnant==null || $scope.Category.pregnant=="" || 
+          $scope.Category.blood==null || $scope.Category.blood=="" ||
+          $scope.Category.fever==null || $scope.Category.fever=="" ||
+          $scope.Category.vaginal==null || $scope.Category.vaginal=="" ||
+          $scope.Category.flank==null || $scope.Category.flank=="") {supersonic.ui.dialog.alert("In order to submit, you must answer each and every question, leaving none blank.");return;}
+
         var UTIcase = Parse.Object.extend("Case");
         var utiCase = new UTIcase();
         
@@ -18,7 +25,7 @@ angular
         utiCase.set("questionnaire",JSON.stringify($scope.Category));
                 utiCase.save(null, {     //save profile object to database
                 success: function(user) {
-                supersonic.ui.dialog.alert("New case submitted");
+                supersonic.ui.dialog.alert("Succesfully Submitted! A Nurse Should Get Back to You Shortly");
                 $scope.$apply();
                 },
                   error: function(user, error) {
@@ -26,6 +33,7 @@ angular
                   }
               });
           $scope.refresh();
+          supersonic.ui.layers.pop();
     };
 
 
