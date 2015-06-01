b47f616d0372fd9317e073750e5d5943edcc55ed
gwcmk <gregwcmkim@gmail.com>
2015-06-01 16:13:57 -0500

    Fixed updating profiles

--- app/example/scripts/ProfileController.js
+++ app/example/scripts/ProfileController.js
@@ -11,14 +11,14 @@ angular
       };
 
   		$scope.submit =function(){
-  			if ($scope.Profile.firstName==null || $scope.Profile.firstName=="") {supersonic.ui.dialog.alert("First Name Needed");return;}
-  			else if ($scope.Profile.lastName==null || $scope.Profile.lastName=="") {supersonic.ui.dialog.alert("Last Name Needed");return;}
-  			else if ($scope.Profile.gender==null||$scope.Profile.gender=="") {supersonic.ui.dialog.alert("Gender Needed");return;}
-  			else if ($scope.Profile.race==null) {supersonic.ui.dialog.alert("Race Needed");return;}
-  			else if ($scope.Profile.streetAddress==null||$scope.Profile.streetAddress=="") {supersonic.ui.dialog.alert("Street Address Needed");return;}
-  			else if ($scope.Profile.city==null ||$scope.Profile.city=="") {supersonic.ui.dialog.alert("City Needed");return;}
-  			else if ($scope.Profile.state==null) {supersonic.ui.dialog.alert("State Needed");return;}
-  			else if ($scope.Profile.zipCode==null||$scope.Profile.zipCode=="") {supersonic.ui.dialog.alert("Zip Code Needed");return;};
+  			// if ($scope.Profile.firstName==null || $scope.Profile.firstName=="") {supersonic.ui.dialog.alert("First Name Needed");return;}
+  			// else if ($scope.Profile.lastName==null || $scope.Profile.lastName=="") {supersonic.ui.dialog.alert("Last Name Needed");return;}
+  			// else if ($scope.Profile.gender==null||$scope.Profile.gender=="") {supersonic.ui.dialog.alert("Gender Needed");return;}
+  			// else if ($scope.Profile.race==null) {supersonic.ui.dialog.alert("Race Needed");return;}
+  			// else if ($scope.Profile.streetAddress==null||$scope.Profile.streetAddress=="") {supersonic.ui.dialog.alert("Street Address Needed");return;}
+  			// else if ($scope.Profile.city==null ||$scope.Profile.city=="") {supersonic.ui.dialog.alert("City Needed");return;}
+  			// else if ($scope.Profile.state==null) {supersonic.ui.dialog.alert("State Needed");return;}
+  			// else if ($scope.Profile.zipCode==null||$scope.Profile.zipCode=="") {supersonic.ui.dialog.alert("Zip Code Needed");return;};
 
   			$scope.currentUser.set("profile",JSON.stringify($scope.Profile));
   				$scope.currentUser.save(null, {     //save profile object to database
